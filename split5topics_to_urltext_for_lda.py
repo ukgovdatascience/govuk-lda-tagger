992923d5b310fe3fdf1d0ba2f31d266ebad5a60b
Ellie King <ellie.king@digital.cabinet-office.gov.uk>
2017-07-26 12:24:53 +0100

    Add working script using argparse to split top level into 5 url, text csv files

--- split5topics_to_urltext_for_lda.py
+++ split5topics_to_urltext_for_lda.py
@@ -2,6 +2,7 @@ import csv
 import pandas as pd
 import re #regular expression
 import click
+import argparse
 
 """
 This module/script takes the tagged url output from train_LDA with 5 topics, splits it into 5 groups and merges each group with original data to produce separate url, text.csvs ready to run LDA on
@@ -12,18 +13,26 @@ python create_bigrams.py input/test_urltext.csv input/test_bigrams.csv
 __author__ = "Ellie King"
 __copyright__ = "Government Digital Service, 10/07/2017"
 
-@click.command()
-@click.option('--preLDA_fpath', prompt='enter original url/text filepath .csv',
-              help='filepath .csv for the LDA input from top level hierarchy with 5 topics')
-@click.option('--tagged_fpath', prompt='enter tags filepath .csv',
-              help='filepath .csv for the LDA output from top level hierarchy with 5 topics')
-def read_data(preLDA_fpath = '--preLDA_fpath', tagged_fpath = '--tagged_fpath'):
+parser = argparse.ArgumentParser(description=__doc__)
+
+parser.add_argument(
+    '--preLDA_fpath', dest='prelda_filename', metavar='FILENAME', default=None,
+    help='import original url text data in csv'
+)
+
+parser.add_argument(
+    '--tagged_fpath', dest='taggedurls_filename', metavar='FILENAME', default=None,
+    help='import tagged urls in csv output from top level of hierarchy, first LDA'
+)
+
+
+def read_data(preLDA_fpath, tagged_fpath):
     """Function to read in original data and tagged documents after LDA with 5 topics"""
-    df_tag5 = pd.read_csv('tagged_fpath', header=None, names=list('abcdefgh'))
-    df_preLDA = pd.read_csv('preLDA_fpath')
+    df_tag5 = pd.read_csv(tagged_fpath, header=None, names=list('abcdefgh'))
+    df_preLDA = pd.read_csv(preLDA_fpath)
     return(df_tag5, df_preLDA)
 
-def clean_tagged_urls(self, df_tag5):
+def clean_tagged_urls(df_tag5):
     """function to clean the dataframe to result in url 
     topic_id cols"""
     #drop the leading parentheses
@@ -32,18 +41,18 @@ def clean_tagged_urls(self, df_tag5):
     df2_tag5 =  df_tag5.drop(df_tag5.columns[[2, 3, 4, 5, 6, 7]], axis=1)
     # name the remaining two columns
     df2_tag5.columns = ['url', 'topic_id']
-    return(df_tag5_clean)
+    return(df2_tag5)
 
-def split_urls_by_topic(self, df_tag5_clean):
+def split_urls_by_topic(df_tag5_clean):
     #split dataframe into 5 separate dfs filtered on topic_id
-    df_first_topic = df2_tag5.loc[df2_tag5['topic_id'] == '0']
-    df_second_topic = df2_tag5.loc[df2_tag5['topic_id'] == '1']
-    df_third_topic = df2_tag5.loc[df2_tag5['topic_id'] == '2']
-    df_fourth_topic = df2_tag5.loc[df2_tag5['topic_id'] == '3']
-    df_fifth_topic = df2_tag5.loc[df2_tag5['topic_id'] == '4']
+    df_first_topic = df_tag5_clean.loc[df_tag5_clean['topic_id'] == '0']
+    df_second_topic = df_tag5_clean.loc[df_tag5_clean['topic_id'] == '1']
+    df_third_topic = df_tag5_clean.loc[df_tag5_clean['topic_id'] == '2']
+    df_fourth_topic = df_tag5_clean.loc[df_tag5_clean['topic_id'] == '3']
+    df_fifth_topic = df_tag5_clean.loc[df_tag5_clean['topic_id'] == '4']
     return(df_first_topic, df_second_topic, df_third_topic,  df_fourth_topic, df_fifth_topic)
     
-def get_text_by_merging(self, df_first_topic, df_second_topic, df_third_topic,  df_fourth_topic, df_fifth_topic, df_preLDA):
+def get_text_by_merging(df_first_topic, df_second_topic, df_third_topic,  df_fourth_topic, df_fifth_topic, df_preLDA):
     #merge on the index column to get text back from original url, text data
     df_first = pd.merge(df_first_topic, df_preLDA, left_index = True, right_index = True , indicator = True)
     df_second = pd.merge(df_second_topic, df_preLDA, left_index = True, right_index = True , indicator = True)
@@ -52,44 +61,86 @@ def get_text_by_merging(self, df_first_topic, df_second_topic, df_third_topic,  
     df_fifth = pd.merge(df_fifth_topic, df_preLDA, left_index = True, right_index = True , indicator = True)
     return(df_first, df_second, df_third,  df_fourth, df_fifth)
 
-def tidy_to_urltext(self, df_first, df_second, df_third,  df_fourth, df_fifth):
+def tidy_to_urltext(df_first, df_second, df_third, df_fourth, df_fifth):
 
     #keep only the url and text columns of the documents tagged with each topic
-    df_first =  df_first.drop(df_first.columns[[1, 2, 4]], axis=1)
-    df_second =  df_second.drop(df_second.columns[[1, 2, 4]], axis=1)
-    df_third =  df_third.drop(df_third.columns[[1, 2, 4]], axis=1)
-    df_fourth =  df_fourth.drop(df_fourth.columns[[1, 2, 4]], axis=1)
-    df_fifth =  df_fifth.drop(df_fifth.columns[[1, 2, 4]], axis=1)
+    df_first_tidy =  df_first.drop(df_first.columns[[1, 2, 4]], axis=1)
+    df_second_tidy =  df_second.drop(df_second.columns[[1, 2, 4]], axis=1)
+    df_third_tidy =  df_third.drop(df_third.columns[[1, 2, 4]], axis=1)
+    df_fourth_tidy =  df_fourth.drop(df_fourth.columns[[1, 2, 4]], axis=1)
+    df_fifth_tidy =  df_fifth.drop(df_fifth.columns[[1, 2, 4]], axis=1)
 
     #rename columns as they will be expected in train_LDA.py
-    df_first.columns = ['url', 'text']
-    df_second.columns = ['url', 'text']
-    df_third.columns = ['url', 'text']
-    df_fourth.columns = ['url', 'text']
-    df_fifth.columns = ['url', 'text']
-    return(df_first, df_second, df_third,  df_fourth, df_fifth)
+    df_first_tidy.columns = ['url', 'text']
+    df_second_tidy.columns = ['url', 'text']
+    df_third_tidy.columns = ['url', 'text']
+    df_fourth_tidy.columns = ['url', 'text']
+    df_fifth_tidy.columns = ['url', 'text']
+    return(df_first_tidy, df_second_tidy, df_third_tidy,  df_fourth_tidy, df_fifth_tidy)
 
-def write_to_5_csvs(self, df_first, df_second, df_third,  df_fourth, df_fifth):
-    df_first.to_csv('first_top_PM.csv', index = False)
-    df_second.to_csv('second_top_PM.csv', index = False)
-    df_third.to_csv('third_top_PM.csv', index = False)
-    df_fourth.to_csv('fourth_top_PM.csv', index = False)
-    df_fifth.to_csv('fifth_top_PM.csv', index = False)
+def write_to_5_csvs(df_first, df_second, df_third,  df_fourth, df_fifth):
+    df_first.to_csv(
+        'enviro_experiments/DATA_environment_2017/dendro/first_top_PM.csv', index = False
+        )
+    df_second.to_csv(
+        'enviro_experiments/DATA_environment_2017/dendro/second_top_PM.csv', index = False
+        )
+    df_third.to_csv(
+        'enviro_experiments/DATA_environment_2017/dendro/third_top_PM.csv', index = False
+        )
+    df_fourth.to_csv(
+        'enviro_experiments/DATA_environment_2017/dendro/fourth_top_PM.csv', index = False)
+    df_fifth.to_csv(
+        'enviro_experiments/DATA_environment_2017/dendro/fifth_top_PM.csv', index = False)
 
     return(df_tag5, df_preLDA)
 
 if __name__ == '__main__':
-    df_tag5, df_preLDA = read_data()
+    args = parser.parse_args()
 
+    print("Loading input file {}".format(args.prelda_filename))
+    print("Loading input file {}".format(args.taggedurls_filename))
+    df_tag5, df_preLDA = read_data(
+        preLDA_fpath = args.prelda_filename, 
+        tagged_fpath = args.taggedurls_filename,
+        )
+
+    print(df_preLDA.head(10))
+
+    print("Cleaning tags")
     df_tag5_clean = clean_tagged_urls(df_tag5)
 
+    print("Splitting by topic")
     df_first_topic, df_second_topic, df_third_topic,  df_fourth_topic, df_fifth_topic = split_urls_by_topic(df_tag5_clean)
 
-    df_first, df_second, df_third,  df_fourth, df_fifth = get_text_by_merging(df_first_topic, df_second_topic, df_third_topic,  df_fourth_topic, df_fifth_topic, df_preLDA)
+    print("Merging to text file")
+    df_first, df_second, df_third,  df_fourth, df_fifth = get_text_by_merging(df_first_topic, 
+        df_second_topic, 
+        df_third_topic,  
+        df_fourth_topic, 
+        df_fifth_topic, 
+        df_preLDA
+        )
 
-    df_second_tidy, df_third_tidy,  df_fourth_tidy, df_fifth_tidy = tidy_to_urltext(df_first, df_second, df_third,  df_fourth, df_fifth)
+    print(df_fifth.head(10))
 
-    write_to_5_csvs(df_first_tidy, df_second_tidy, df_third_tidy,  df_fourth_tidy, df_fifth_tidy)
+    print("Tidying up")
+    df_first_tidy, df_second_tidy, df_third_tidy, df_fourth_tidy, df_fifth_tidy = tidy_to_urltext(
+        df_first, 
+        df_second, 
+        df_third, 
+        df_fourth, 
+        df_fifth
+        )
+
+    print("Write to file")
+    write_to_5_csvs(
+        df_first_tidy, 
+        df_second_tidy, 
+        df_third_tidy,  
+        df_fourth_tidy, 
+        df_fifth_tidy
+        )
 
 
 
